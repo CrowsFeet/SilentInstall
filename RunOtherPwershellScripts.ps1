f5a8dbf9c2d1649d299a5f102130608385e1ca73
AG <36303884+CrowsFeet@users.noreply.github.com>
2025-03-13 10:34:38 +0000

    Add files via upload

---
+++ RunOtherPwershellScripts.ps1
@@ -0,0 +1,5 @@
+﻿#Invoke-Expression -Command .\C:\Users\ecicloudserviceuser\Downloads\Silent-Install-SSMS.ps1
+#Invoke-Item (start powershell ((Split-Path $MyInvocation.InvocationName) + "\Silent-Install-SSMS.ps1")
+
+start-process "powershell.exe" -ArgumentList '-File',"C:\Users\ecicloudserviceuser\Downloads\Silent-Install-SSMS.ps1"
+
