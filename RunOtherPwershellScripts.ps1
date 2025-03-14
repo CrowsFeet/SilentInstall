4d3321c36c76360b1ce469293af8ba796da82d2d
AG <36303884+CrowsFeet@users.noreply.github.com>
2025-03-14 14:35:11 +0000

    Update RunOtherPwershellScripts.ps1

--- RunOtherPwershellScripts.ps1
+++ RunOtherPwershellScripts.ps1
@@ -1,5 +1,15 @@
 ﻿#Invoke-Expression -Command .\C:\Users\ecicloudserviceuser\Downloads\Silent-Install-SSMS.ps1
 #Invoke-Item (start powershell ((Split-Path $MyInvocation.InvocationName) + "\Silent-Install-SSMS.ps1")
 
+$proc = start-process "powershell.exe" -ArgumentList '-File',"C:\Users\ecicloudserviceuser\Downloads\InstallSQLServer.ps1 -saPwd AGTest01 -InstanceName MSSQLSERVER" -wait
+
+$proc.waitforexit()
+
+#Start-Sleep -s 120
+
 start-process "powershell.exe" -ArgumentList '-File',"C:\Users\ecicloudserviceuser\Downloads\Silent-Install-SSMS.ps1"
+
+
+
+#start-process "powershell.exe" -ArgumentList '-File',"C:\Users\ecicloudserviceuser\Downloads\Silent-Install-SSMS.ps1"
 
