cf5a7af213727d1b48e6cc1b701a33300ece1360
AG <36303884+CrowsFeet@users.noreply.github.com>
2025-03-19 13:50:47 +0000

    Add files via upload

---
+++ SilentInstallProductParameters.ps1
@@ -0,0 +1,11 @@
+﻿# Install FMAudit
+.\InstallSQLServer.ps1 SQL2019
+    -saPwd SuperStr0ngPassword 
+    -Instancename MSSQLSERVER
+    -AutoMaxMemory 
+    -InstanceRootDir D:\ 
+    -UserDataRootDir E:\Data\
+    -UserTLogRootDir L:\Log\
+    -TempdbDataRootDir T:\Tempdb\
+    -BackupRootDir E:\Backup\
+ 
