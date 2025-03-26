3b5b360b5305c5148b329980f01c34730f183f30
AG <36303884+CrowsFeet@users.noreply.github.com>
2025-03-26 11:15:38 +0000

    Update SilentInstallProductParameters.ps1

--- SilentInstallProductParameters.ps1
+++ SilentInstallProductParameters.ps1
@@ -19,3 +19,14 @@
     -UserTLogRootDir L:\Log\
     -TempdbDataRootDir F:\Tempdb\
     -BackupRootDir D:\Backup\
+
+# Install ECInteractive
+.\InstallSQLServer.ps1 SQL2019
+    -saPwd SuperStr0ngPassword 
+    -Instancename MSSQLSERVER
+    -AutoMaxMemory 
+    -InstanceRootDir E:\ 
+    -UserDataRootDir E:\Data\
+    -UserTLogRootDir L:\Log\
+    -TempdbDataRootDir T:\Tempdb\
+    -BackupRootDir E:\Backup\
